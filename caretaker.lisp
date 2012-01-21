6a889d2297692347147eb85d95684e30a42b734f
Pierre THierry <pierre@nothos.net>
2012-01-21 17:14:38 +0100

    Draft code of HTTP caretaker

---
+++ caretaker.lisp
@@ -0,0 +1,15 @@
+(defun make-http-caretaker (uri)
+  (let ((web-client (lambda () (drakma:http-request uri)))) 
+    (list
+     (lambda ()
+       (multiple-value-bind (data code headers)
+	   (funcall web-client)
+	 (setf (return-code*) code)
+	 (cif str-length (cdr (find :content-length headers :key #'car))
+	      (setf (content-length*) (parse-integer str-length :junk-allowed t)))
+	 (cif type (cdr (find :content-type headers :key #'car))
+	      (setf (content-type*) type))
+	 data))
+     (lambda ()
+       (setf web-client
+	     (lambda () (values "" 501 nil)))))))
